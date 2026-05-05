30b04119d08ed0c3c53a871699c1a1b9ac34ae7d
aruzhan-nurbai <aruzhan.nurbai@std.agakaz.kz>
2026-05-05 09:35:07 +0500

    Цвет при достижении B

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import time
 import hashlib
 import os
 import json
+import time
 
 # ----------------------------
 # 🟢 КОНСТАНТЫ
@@ -421,6 +422,7 @@ while True:
         print("🎯 Reached B! RETURN TO A!")
         print(f"🟢 Теперь будут появляться препятствия!")
         going_forward = False
+        hero.color('yellow')
         
         log.append({
             "event": "reached_goal_B",
